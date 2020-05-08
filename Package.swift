07a68f932a29d22a2d642a0a2c509be8f370e5f9
Jaehong Kang <j.kang@ridi.com>
2020-05-08 10:22:50 +0900

    Change library as dynamic

--- Package.swift
+++ Package.swift
@@ -12,6 +12,11 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "RIDIFoundation",
+            type: .dynamic,
+            targets: ["RIDIFoundation"]
+        ),
+        .library(
+            name: "RIDIFoundation-auto",
             targets: ["RIDIFoundation"]
         )
     ],
