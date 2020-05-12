ddbca47f644fd42eb120e147e08060c7cd039152
Jaehong Kang <j.kang@ridi.com>
2020-05-12 17:30:52 +0900

    Add additional tests

--- Tests/RIDIFoundationTests/XML/XMLTests.swift
+++ Tests/RIDIFoundationTests/XML/XMLTests.swift
@@ -6,19 +6,23 @@ final class XMLTests: XCTestCase {
     func testXMLInitNote() throws {
         let xmlData = try Data(contentsOf: URL(fileURLWithPath: #file).deletingLastPathComponent().appendingPathComponent("note.xml"))
 
-        let xmlDocument = try XMLDocument(data: xmlData)
+        measureMetrics([.wallClockTime], automaticallyStartMeasuring: false) {
+            startMeasuring()
+            let xmlDocument = try! XMLDocument(data: xmlData)
+            stopMeasuring()
 
-        XCTAssertEqual(xmlDocument.children?.count, 1)
-        XCTAssertEqual(xmlDocument.children?[0].name, "note")
-        XCTAssertEqual(xmlDocument.children?[0].children?.count, 4)
-        XCTAssertEqual(xmlDocument.children?[0].children?[0].name, "to")
-        XCTAssertEqual(xmlDocument.children?[0].children?[0].stringValue, "Tove")
-        XCTAssertEqual(xmlDocument.children?[0].children?[1].name, "from")
-        XCTAssertEqual(xmlDocument.children?[0].children?[1].stringValue, "Jani")
-        XCTAssertEqual(xmlDocument.children?[0].children?[2].name, "heading")
-        XCTAssertEqual(xmlDocument.children?[0].children?[2].stringValue, "Reminder")
-        XCTAssertEqual(xmlDocument.children?[0].children?[3].name, "body")
-        XCTAssertEqual(xmlDocument.children?[0].children?[3].stringValue, "Don't forget me this weekend!")
+            XCTAssertEqual(xmlDocument.children?.count, 1)
+            XCTAssertEqual(xmlDocument.children?[0].name, "note")
+            XCTAssertEqual(xmlDocument.children?[0].children?.count, 4)
+            XCTAssertEqual(xmlDocument.children?[0].children?[0].name, "to")
+            XCTAssertEqual(xmlDocument.children?[0].children?[0].stringValue, "Tove")
+            XCTAssertEqual(xmlDocument.children?[0].children?[1].name, "from")
+            XCTAssertEqual(xmlDocument.children?[0].children?[1].stringValue, "Jani")
+            XCTAssertEqual(xmlDocument.children?[0].children?[2].name, "heading")
+            XCTAssertEqual(xmlDocument.children?[0].children?[2].stringValue, "Reminder")
+            XCTAssertEqual(xmlDocument.children?[0].children?[3].name, "body")
+            XCTAssertEqual(xmlDocument.children?[0].children?[3].stringValue, "Don't forget me this weekend!")
+        }
     }
 
     func testXMLSubscriptByXPathNote() throws {
@@ -44,26 +48,30 @@ final class XMLTests: XCTestCase {
     func testXMLInitContainer() throws {
         let xmlData = try Data(contentsOf: URL(fileURLWithPath: #file).deletingLastPathComponent().appendingPathComponent("container.xml"))
 
-        let xmlDocument = try XMLDocument(data: xmlData)
+        measureMetrics([.wallClockTime], automaticallyStartMeasuring: false) {
+            startMeasuring()
+            let xmlDocument = try! XMLDocument(data: xmlData)
+            stopMeasuring()
 
-        XCTAssertEqual(xmlDocument.children?.count, 1)
+            XCTAssertEqual(xmlDocument.children?.count, 1)
 
-        let containerElement = xmlDocument.children?[0] as? RIDIFoundation.XMLElement
-        XCTAssertEqual(containerElement?.name, "container")
-        XCTAssertEqual(containerElement?.attributes?["xmlns"].first?.stringValue, "urn:oasis:names:tc:opendocument:xmlns:container")
-        XCTAssertEqual(containerElement?.attributes?["version"].first?.stringValue, "1.0")
-        XCTAssertEqual(containerElement?.children?.count, 1)
+            let containerElement = xmlDocument.children?[0] as? RIDIFoundation.XMLElement
+            XCTAssertEqual(containerElement?.name, "container")
+            XCTAssertEqual(containerElement?.attributes?["xmlns"].first?.stringValue, "urn:oasis:names:tc:opendocument:xmlns:container")
+            XCTAssertEqual(containerElement?.attributes?["version"].first?.stringValue, "1.0")
+            XCTAssertEqual(containerElement?.children?.count, 1)
 
-        let rootfilesElement = containerElement?.children?[0] as? XMLElement
-        XCTAssertEqual(rootfilesElement?.name, "rootfiles")
-        XCTAssertEqual(rootfilesElement?.attributes?.count, 0)
-        XCTAssertEqual(rootfilesElement?.children?.count, 1)
+            let rootfilesElement = containerElement?.children?[0] as? XMLElement
+            XCTAssertEqual(rootfilesElement?.name, "rootfiles")
+            XCTAssertEqual(rootfilesElement?.attributes?.count, 0)
+            XCTAssertEqual(rootfilesElement?.children?.count, 1)
 
-        let rootfileElement = rootfilesElement?.children?[0] as? XMLElement
+            let rootfileElement = rootfilesElement?.children?[0] as? XMLElement
 
-        XCTAssertEqual(rootfileElement?.name, "rootfile")
-        XCTAssertEqual(rootfileElement?.attributes?["full-path"].first?.stringValue, "OEBPS/content.opf")
-        XCTAssertEqual(rootfileElement?.attributes?["media-type"].first?.stringValue, "application/oebps-package+xml")
+            XCTAssertEqual(rootfileElement?.name, "rootfile")
+            XCTAssertEqual(rootfileElement?.attributes?["full-path"].first?.stringValue, "OEBPS/content.opf")
+            XCTAssertEqual(rootfileElement?.attributes?["media-type"].first?.stringValue, "application/oebps-package+xml")
+        }
     }
 
     func testXMLSubscriptByXPathContainer() throws {
@@ -71,15 +79,27 @@ final class XMLTests: XCTestCase {
 
         let xmlDocument = try XMLDocument(data: xmlData)
 
-        let rootfileNode = xmlDocument[xPath: "//rootfile"].first
-        XCTAssertNotNil(rootfileNode)
+        let rootfileNodes = xmlDocument[xPath: "//rootfile"]
+        XCTAssertEqual(rootfileNodes.count, 1)
 
-        let rootfileElement = rootfileNode as? XMLElement
+        let rootfileElement = rootfileNodes.first as? XMLElement
         XCTAssertNotNil(rootfileElement)
 
         XCTAssertEqual(rootfileElement?.name, "rootfile")
         XCTAssertEqual(rootfileElement?.attributes?["full-path"].first?.stringValue, "OEBPS/content.opf")
         XCTAssertEqual(rootfileElement?.attributes?["media-type"].first?.stringValue, "application/oebps-package+xml")
+    }
+
+    func testExcessiveSpineItemsXMLInit() throws {
+        let xmlData = try Data(contentsOf: URL(fileURLWithPath: #file).deletingLastPathComponent().appendingPathComponent("excessive-spine-items.xml"))
+
+        measureMetrics([.wallClockTime], automaticallyStartMeasuring: false) {
+            startMeasuring()
+            let xmlDocument = try! XMLDocument(data: xmlData)
+            stopMeasuring()
+
+            XCTAssertEqual(xmlDocument.children?.count, 1)
+        }
     }
 
     static var allTests = [
