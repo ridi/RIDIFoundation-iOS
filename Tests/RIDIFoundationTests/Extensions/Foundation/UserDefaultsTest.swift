34e8b6ead635d62b19690a3ef295d199544b58c1
Jaehong Kang <j.kang@ridi.com>
2020-05-22 11:33:52 +0900

    Add Observation

--- Tests/RIDIFoundationTests/Extensions/Foundation/UserDefaultsTest.swift
+++ Tests/RIDIFoundationTests/Extensions/Foundation/UserDefaultsTest.swift
@@ -154,6 +154,48 @@ final class UserDefaultsTests: XCTestCase {
         )
     }
 
+    func testObservation() {
+        let key = UserDefaults.Key<String>(UUID().uuidString)
+        let userDefaults = UserDefaults.standard
+
+        let newValue = UUID().uuidString
+
+        let expectation = XCTestExpectation(description: "Notified")
+        expectation.expectedFulfillmentCount = 1
+
+        let observation = userDefaults.observe(key) {
+            XCTAssertEqual($0, userDefaults)
+            XCTAssertEqual($1.newValue, newValue)
+
+            expectation.fulfill()
+        }
+
+        userDefaults[key] = newValue
+
+        wait(for: [expectation], timeout: 5.0)
+    }
+
+    func testObservationPrior() {
+        let key = UserDefaults.Key<String>(UUID().uuidString)
+        let userDefaults = UserDefaults.standard
+
+        let newValue = UUID().uuidString
+
+        let expectation = XCTestExpectation(description: "Notified")
+        expectation.expectedFulfillmentCount = 2
+
+        let observation = userDefaults.observe(key, options: [.prior, .new]) {
+            XCTAssertEqual($0, userDefaults)
+            _ = $1
+
+            expectation.fulfill()
+        }
+
+        userDefaults[key] = newValue
+
+        wait(for: [expectation], timeout: 5.0)
+    }
+
     static var allTests = [
         ("testBinding", testBinding),
         ("testSubscript", testSubscript),
