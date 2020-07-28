af923bf4caa2fe2f72ef6b8907de342da1a05c13
Jaehong Kang <j.kang@ridi.com>
2020-07-28 10:56:29 +0900

    Fixes test

--- Tests/RIDIFoundationTests/FileObservation/FileObservationTests.swift
+++ Tests/RIDIFoundationTests/FileObservation/FileObservationTests.swift
@@ -21,11 +21,12 @@ final class FileObservationTests: XCTestCase {
             encoding: .utf8
         )
 
-        fileObservation = nil
-
         try FileManager.default.removeItem(at: tempDirectory)
 
         wait(for: [expectation], timeout: 5.0)
+
+        fileObservation = nil
+        XCTAssert(fileObservation == nil)
     }
 
     func testNilNotify() throws {
@@ -42,6 +43,7 @@ final class FileObservationTests: XCTestCase {
         }
 
         fileObservation = nil
+        XCTAssert(fileObservation == nil)
 
         try FileManager.default.removeItem(at: tempDirectory)
 
